72702798b1151e55c2b0fe6098a2687b9161d4fc
Santi292003 <s.ramirez9@utp.edu.co>
2026-05-07 08:53:16 -0500

    deploy actualizado

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,4 @@ npm run build
 echo "📦 Copiando al servidor..."
 scp -r dist/* testserver@172.16.1.21:/var/www/rueda-competencias/
 
-echo "🔑 Ajustando permisos..."
-ssh testserver@172.16.1.21 "sudo chmod -R 775 /var/www/rueda-competencias && sudo chown -R testserver:www-data /var/www/rueda-competencias"
-
 echo "✅ Desplegado en http://172.16.1.21/rueda/"
